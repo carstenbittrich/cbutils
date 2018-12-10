ba9c9adeae44b331023a88e435e7888f504c7777
Carsten Bittrich <carsten.bittrich@icloud.com>
2018-12-10 13:56:23 +0100

    minor changes to sublime settings and fix for git repo update test

--- dotFiles/zsh/lib/repoUpdateCheck.zsh
+++ dotFiles/zsh/lib/repoUpdateCheck.zsh
@@ -1,10 +1,12 @@
 #!/usr/bin/env zsh
 
 CURDIR=$PWD
-cd $CBUTILS
-if [ `git status | grep -c "nothing to commit"` -eq 0 ]; then
-    # not up to date
-    echo "$CBUTILS has local changes!"
-    git status
+if command -v git > /dev/null; then
+    cd $CBUTILS
+    if [ `git status | grep -c "nothing to commit"` -eq 0 ]; then
+        # not up to date
+        echo "$CBUTILS has local changes!"
+        git status
+    fi
+    cd $CURDIR
 fi
-cd $CURDIR
